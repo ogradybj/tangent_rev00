c5afec773c5347b3f8e64e07265a8de34dbad026
Brendan O'Grady <ogradybj@gmail.com>
2016-01-30 23:27:27 -0600

    Update tangentgame.py
    a few updates, updated comments, player speed is faster than expected. Still need to organize math around orbit mechanics.

--- tangentgame.py
+++ tangentgame.py
@@ -46,16 +46,16 @@ class Ball(pygame.sprite.Sprite):
         Called by update() or the main program loop if there is a collision.
         """
         self.rect.y = random.randrange(screen_height/65)*65
-        self.rect.x = random.randrange(screen_width+10, screen_width+15)
+        self.rect.x = random.randrange(screen_width*2+10, screen_width*2+15)
  
     def update(self, speed):
         """ Called each frame. """
         self.dx = speed
-        # Move block down one pixel
+       
         self.rect.x -= self.dx
  
-        # If block is too far down, reset to top of screen.
-        if self.rect.x < -65:
+        # If ball has moved off screen, reset to new position
+        if self.rect.x < -100:
             self.reset_pos()
  
  
@@ -70,8 +70,8 @@ class Player(Ball):
         #super(pygame.sprite.Sprite, self).__init__()
         pygame.sprite.Sprite.__init__(self)
  
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
+        # Create an image of the ball, and fill it with a color.
+        # May make sense to make an image later on
         self.image = pygame.Surface([width, height])
         self.image.fill(color)
         self.on = False
@@ -82,67 +82,58 @@ class Player(Ball):
         # of rect.x and rect.y
         pygame.draw.circle(self.image, (BLUE), (width/2, height/2), width/2, 5)
         self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(100,600)
-        self.rect.y = random.randrange(50,350)
-        self.xdir = 1
-        self.ydir = 1
+        self.rect.x = random.randrange(350,450)
+        self.rect.y = random.randrange(175,225)
+        self.xdir = 1 #math.sin(3.14159/4)
+        self.ydir = 1 #math.cos(3.14159/4)
 
-        self.dx = speed*2
-        self.dy = speed*2
+        self.dx = speed*1
+        self.dy = speed*1
 
         self.theta = 0
         self.thetav = 0
 
-    def update(self, speed):
-        # Get the current mouse position. This returns the position
-        # as a list of two numbers.
-        #pos = pygame.mouse.get_pos()
- 
-        # Fetch the x and y out of the list,
-        # just like we'd fetch letters out of a string.
-        # Set the player object to the mouse location
-        #self.rect.x = pos[0]
-        #self.rect.y = pos[1]
+    def update(self, speed, zball=None, cluck=False):
         
-        self.dx = speed*3
-        self.dy = speed*3
-        #print(self.on)
-        #if self.on == False:    
+        
 
-        if self.rect.x >= 800:
+
+
+
+        if cluck == True:
+
+            self.thetav = 0.033345*speed 
+        
+            self.theta = math.tan((self.rect.y-(zball.rect.center[1]))/(self.rect.x-(zball.rect.center[0])))
+
+            
+            #self.xdir = 1*math.sin(self.theta)
+            #self.ydir = 1*math.cos(self.theta)
+        
+
+            #self.dx = 30*math.sin(self.thetav)-speed
+            #self.dy = 30*math.cos(self.thetav)
             self.xdir = -1
-        elif self.rect.x <= 0:
-            self.xdir = 1
-        if self.rect.y >= 390:
-            self.ydir = -1
-        elif self.rect.y <= 0:
-            self.ydir = 1
+            self.ydir = 0
+
+        else:
+
+            if self.rect.x >= 790:
+                self.xdir = self.xdir*(-1)
+            elif self.rect.x <= 10:
+                self.xdir = self.xdir*(-1)
+            if self.rect.y >= 390:
+                self.ydir = self.ydir*(-1)
+            elif self.rect.y <= 10:
+                self.ydir = self.ydir*(-1)
 
 
-        self.rect.x = self.rect.x+(self.xdir*self.dx)
-        self.rect.y = self.rect.y+(self.ydir*self.dy)
+        self.dx = speed*1*self.xdir
+        self.dy = speed*1*self.ydir
 
+        self.rect.x = self.rect.x+self.dx
+        self.rect.y = self.rect.y+self.dy
 
-    def orbit(self, speed, ballx, bally):
-        
-        #self.rect.x = self.rect.x-speed
-
-        self.thetav = math.atan(speed/30)
-        
-        self.theta = math.atan((self.rect.y-bally)/(self.rect.x-ballx))
-
-
-        self.xdir = 1*math.sin(self.theta)
-
-        self.ydir = 1*math.cos(self.theta)
-
-        self.dx = 30*math.sin(self.thetav)-speed
-        self.dy = 30*math.cos(self.thetav)
-
-
-
-        self.rect.x = self.rect.x+(self.xdir*self.dx)
-        self.rect.y = self.rect.y+(self.ydir*self.dy)
 
     
 
@@ -165,27 +156,31 @@ ball_list = pygame.sprite.Group()
 # This is a list of every sprite. All blocks and the player block as well.
 all_sprites_list = pygame.sprite.Group()
  
-for i in range(11):
+ #creating all the balls, randomly distributed
+for i in range(15):
     # This represents a blall
     ball = Ball(WHITE, 60, 60, speed)
  
     # Set a random location for the block
-    ball.rect.x = random.randrange(screen_width/65)*65
-    ball.rect.y = random.randrange(screen_height/65)*65
+    ball.rect.x = random.randrange(screen_width*2/65)*65+screen_width
+    ball.rect.y = random.randrange(screen_height/85)*85
  
-    # Add the block to the list of objects
+    # Add the ball to the list of objects
     ball_list.add(ball)
     all_sprites_list.add(ball)
  
-# Create a red player block
+# Create a blue ball for the player
 player = Player(WHITE, 20, 20, speed)
 playerGroup = pygame.sprite.Group()
+#making player group, so that we can simply call
+#playerGroup.draw(screen) otherwise if we add player to
+#the ball sprite group, it will draw underneath the balls
 playerGroup.add(player)
 all_sprites_list.add(player)
  
-# Loop until the user clicks the close button.
+# possibly use these later, still trying to work out orbit
 done = False
-on = False
+on = None
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
@@ -197,38 +192,45 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif event.type == pygame.MOUSEBUTTONUP:# & guy.on == True:
+            
+            player.update(speed, on, False)
  
     # Clear the screen
     screen.fill(WHITE)
     
+    #periodically increase the speed to make the game more difficult
     time += 1
     if time % 300 == 0:
         speed +=.5
         
 
-    # Calls update() method on every sprite in the list
+    # Calls update() method on every ball in the list
     ball_list.update(speed)
-    ii = 0
+
+    #check to see if player collides with any balls
     for ball in ball_list:
+        #if there is a collision start orbiting the ball by
+        #calling player.orbit method
         if pygame.sprite.collide_rect(player, ball):
-            player.orbit(speed, ball.rect.x, ball.rect.y)
+            on = ball
+            player.update(speed, on, True)
+            print(ball.rect.center[0])
+            print(ball.rect.center[1])
 
 
-        #if pygame.sprite.collide_rect(player, ball):
-         #   
-          #  ii +=1
+        #if no collision keep updating like normal
+        else:
+            player.update(speed)  
 
 
-    if ii > 0:
-        player.orbit(speed)
-
-    else:
-        player.update(speed)  
-
-
-
+    #players score is simply amount of time they have survived
     score = time
-    print(score)
+    #print(score)
+    #print(speed)
+    #print(player.ydir)
+    #print(player.xdir)
+
 
  
     # Draw all the sprites
@@ -236,9 +238,9 @@ while not done:
     playerGroup.draw(screen)
  
     # Limit to 20 frames per second
-    clock.tick(20)
+    clock.tick(24)
  
-    # Go ahead and update the screen with what we've drawn.
+    #update the screen with what we've drawn.
     pygame.display.flip()
  
 pygame.quit()
